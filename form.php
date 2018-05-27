f31cf446274538d2a89edc5186cbf2cf6e103646
Shastapalava <33579225+Shastapalava@users.noreply.github.com>
2018-05-26 22:38:24 -0400

    Create form.php

---
+++ form.php
@@ -0,0 +1,33 @@
+<HTML>
+<HEAD>
+<TITLE></TITLE>
+</HEAD>
+<BODY>
+</A></div>
+<h1 align="center">Please enter your trip information here</h1><br>
+</div>
+<form method="post" action="book.php">
+<p>
+<label>Date of the trip(yyyy-mm-dd)</label>
+<input type="text" name="trip_date">
+</p>
+<p>
+ <label>From(station abbriviation)</label>
+ <input type="text" name="trip_start">
+</p>
+<p>
+ <label>To(station abbriviation)</label>
+ <input type="text" name="trip_end">
+</p>
+<p>
+ <label>Time of the day(MOR AFT EVE NIT)</label>
+ <input type="text" name="time_of_day">
+</p>
+<p>
+ <label>Day of the week(MF or SSH)</label>
+  <input type="text" name="day_of_week">
+</p>
+  <input type="submit" value="Go">
+</form>
+</BODY>
+</HTML>
